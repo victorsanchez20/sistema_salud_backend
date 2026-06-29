d38796438a49ac99b0005efa47c97a5624877d7c
Victor <sanchezcueva20va@gmail.com>
2026-06-29 17:56:20 -0500

    feat: Update CORS configuration to allow requests from the production URL

--- src/main/java/com/salud/demo/config/WebConfig.java
+++ src/main/java/com/salud/demo/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("http://localhost:4200",
+                "https://sistema-salud-fisica.vercel.app"
+            )
             .allowedMethods("GET","POST","PUT","DELETE")
             .allowedHeaders("*");
     }
